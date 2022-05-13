8320b8a34ab84f5b132a78e3c197b7cabb821c4d
Anandaswarup Vadapalli <anandaswarup.vadapalli@gmail.com>
2022-05-13 19:23:43 +0530

    Model definition, loss function and evaluation metrics

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-"""Model"""
+"""Model definition, loss function and evaluation metrics"""
 
 import numpy as np
 import torch
@@ -40,3 +40,30 @@ class PhraseBreakPredictor(nn.Module):
         x = self.fc(x)
 
         return F.log_softmax(x, dim=1)
+
+
+def loss_fn(outputs, labels):
+    """Compute the cross-entropy loss; while excluding the loss terms for padding tokens
+
+        Args:
+            outputs (tensor): log softmax output of the model [B * T_max, num_tags]
+            labels (tensor): Ground truth labels where eac:qh element is a label index in [0, 1, ....num_tags - 1] or
+                             -1 in the case of padding tokens [B, T_max]
+        
+        Returns:
+            loss (tensor): cross-entropy loss for all tokens in the batch
+    """
+    # [B, T_max] -> [B * T_max]
+    labels = labels.view(-1).contiguous()
+
+    # Generate mask for padding terms
+    mask = (labels >= 0).float()
+
+    # Since padding tokens have index -1, we need to convert them to a positive number
+    labels = labels % outputs.shape[1]
+
+    # Total number of tokens
+    num_tokens = int(torch.sum(mask))
+
+    # Compute cross-entropy loss for all tokens (except padding tokens)
+    return -torch.sum(outputs[range(outputs.shape[0]), labels] * mask) / num_tokens
