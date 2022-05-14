6a181b2827cfa842c06caca5afe7cae235ab73a4
Anandaswarup Vadapalli <anandaswarup.vadapalli@gmail.com>
2022-05-14 21:49:03 +0530

    Utilities

--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@
 
 import json
 import os
-import shutil
 
 import torch
 
@@ -46,30 +45,28 @@ def save_dict_to_json(d, filename):
         json.dump(d, file_writer, indent=4)
 
 
-def save_checkpoint(checkpoint_state, is_best, checkpoint_dir):
+def save_checkpoint(checkpoint_dir, model, optimizer, epoch):
     """Saves model and training parameters (state dict) as a checkpoint
     """
-    filepath = os.path.join(checkpoint_dir, "last.pth")
-    if not os.path.exists(checkpoint_dir):
-        print(f"Checkpoint dir does not exist. Making directory {checkpoint_dir}")
-        os.makedirs(checkpoint_dir)
-    else:
-        print("Using existing checkpoint dir")
+    checkpoint_state = {
+        "model": model.state_dict(),
+        "optmizer": optimizer.state_dict(),
+        "epoch": epoch,
+    }
 
-    torch.save(checkpoint_state, filepath)
-    if is_best:
-        shutil.copy(filepath, os.path.join(checkpoint_dir, "best.pth"))
+    checkpoint_path = os.path.join(checkpoint_dir, f"model_epoch{epoch:04d}.pth")
+
+    torch.save(checkpoint_state, checkpoint_path)
 
 
-def load_checkpoint(checkpoint_path, model, optimizer=None):
+def load_checkpoint(checkpoint_path, model, optimizer):
     """Loads model parameters (state dict) from checkpoint_path
     """
-    if not os.path.exists(checkpoint_path):
-        raise (f"Checkpoint {checkpoint_path} does not exist")
+    print(f"Loading checkpoint: {checkpoint_path} from disk")
 
     checkpoint = torch.load(checkpoint_path)
-    model.load_state_dict(checkpoint["state_dict"])
 
-    if optimizer:
-        optimizer.load_state_dict(checkpoint["optim_dict"])
-    return checkpoint
+    model.load_state_dict(checkpoint["model"])
+    optimizer.load_state_dict(checkpoint["optimizer"])
+
+    return checkpoint["epoch"]
