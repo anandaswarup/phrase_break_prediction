5e1f1e6df2d5a1260d7d635c8960f1c0a16074a3
anandaswarup <anandaswarup.vadapalli@gmail.com>
2023-02-18 12:14:20 +0530

    Model definition for fine tuned BERT model with a token classification head

--- model/bert.py
+++ model/bert.py
@@ -1,33 +1,43 @@
 """Model definition for fine tuned BERT model with a token classification head"""
 
 import torch.nn as nn
+from transformers import BertModel
 
 
 class BERTPhraseBreakPredictor(nn.Module):
     """Model definition for fine tuned BERT model with a token classification head"""
 
-    def __init__(self, bert_model, num_puncs):
+    def __init__(self, cfg, num_puncs):
         """Instantiate the model"""
         super().__init__()
 
-        self.bert_model = bert_model
-        self.bert_hidden_size = self.bert_model.config.hidden_size
-        self.num_puncs = num_puncs
+        # BERT model
+        self.bert_model = BertModel.from_pretrained(cfg["bert_model_name"], add_pooling_layer=False)
 
         # Dropout layer
         self.dropout_layer = nn.Dropout(0.5)
 
         # Output (fully connected layer)
-        self.output_layer = nn.Linear(in_features=self.bert_hidden_size, out_features=self.num_puncs)
+        self.output_layer = nn.Linear(in_features=self.bert_model.config.hidden_size, out_features=num_puncs)
 
     def forward(self, texts, attention_masks):
         """Forward pass"""
-        # [[B, T_max], [B, T_max]] -> [B, T_max, bert_hidden_size]
-        bert_outputs = self.bert_model(texts, attention_masks)
-        bert_outputs = bert_outputs.last_hidden_state
-        bert_outputs = self.dropout_layer(bert_outputs)
+        outputs = self.bert_model(
+            input_ids=texts,
+            attention_mask=attention_masks,
+            token_type_ids=None,
+            position_ids=None,
+            head_mask=None,
+            inputs_embeds=None,
+            output_attentions=None,
+            output_hidden_states=None,
+            return_dict=None,
+        )
 
-        # [B, T_max, bert_hidden_size] -> [B, T_max, num_puncs]
-        logits = self.output_layer(bert_outputs)
+        sequence_output = outputs[0]
+
+        sequence_output = self.dropout(sequence_output)
+
+        logits = self.output_layer(sequence_output)
 
         return logits
