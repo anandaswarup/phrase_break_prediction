518ff5aa8d5289f65d95b5a5ce2c6fa96efdfa8e
anandaswarup <anandaswarup.vadapalli@gmail.com>
2023-02-22 22:49:58 +0530

    Model definition for fine tuned BERT model with a token classification head

--- model/bert.py
+++ model/bert.py
@@ -1,42 +1,22 @@
 """Model definition for fine tuned BERT model with a token classification head"""
 
 import torch.nn as nn
-from transformers import BertModel
+from transformers import BertForTokenClassification
 
 
 class BERTPhraseBreakPredictor(nn.Module):
-    """Model definition for fine tuned BERT model with a token classification head"""
+    """Model definition for fine tuned BERT model with token classification head"""
 
     def __init__(self, cfg, num_puncs):
         """Instantiate the model"""
         super().__init__()
 
-        # BERT model
-        self.bert_model = BertModel.from_pretrained(cfg["bert_model_name"], add_pooling_layer=False)
+        self.num_puncs = num_puncs
+        self.bert_model = BertForTokenClassification.from_pretrained(cfg["bert_model_name"], num_labels=num_puncs)
 
-        # Dropout layer
-        self.dropout_layer = nn.Dropout(0.5)
-
-        # Output (fully connected layer)
-        self.output_layer = nn.Linear(in_features=self.bert_model.config.hidden_size, out_features=num_puncs)
-
-    def forward(self, texts, attention_masks):
+    def forward(self, texts, attention_mask, labels):
         """Forward pass"""
-        outputs = self.bert_model(
-            input_ids=texts,
-            attention_mask=attention_masks,
-            token_type_ids=None,
-            position_ids=None,
-            head_mask=None,
-            inputs_embeds=None,
-            output_attentions=None,
-            output_hidden_states=None,
-            return_dict=None,
-        )
+        outputs = self.bert_model(texts, attention_mask=attention_mask, labels=labels)
+        loss, logits = outputs[0], outputs[1]
 
-        sequence_output = outputs[0]
-        sequence_output = self.dropout_layer(sequence_output)
-
-        logits = self.output_layer(sequence_output)
-
-        return logits
+        return loss, logits
