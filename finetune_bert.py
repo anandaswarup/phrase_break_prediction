890886d63e4d08306dcfbb8139ff1cb68401184e
anandaswarup <anandaswarup.vadapalli@gmail.com>
2023-02-25 21:39:57 +0530

    Finetune BERT model with a token classification head for phrase break prediction

--- finetune_bert.py
+++ finetune_bert.py
@@ -19,35 +19,53 @@ def _write_log_to_file(log, filename):
     print(f"Written log file: {filename}")
 
 
-def _eval(model, device, loader, num_puncs):
+def _remove_predictions_for_padded_tokens(pred_puncs, puncs):
+    """Remove predictions corresponding to padded tokens"""
+    pred_puncs_without_padded = []
+    puncs_without_padded = []
+
+    for pred_punc, punc in zip(pred_puncs, puncs):
+        if punc > -100:
+            pred_puncs_without_padded.append(pred_punc)
+            puncs_without_padded.append(punc)
+
+    return pred_puncs_without_padded, puncs_without_padded
+
+
+def _eval(model, device, loader):
     """Evaluate the model"""
     model.eval()
+
     with torch.no_grad():
-        puncs_predictions, puncs_correct = [], []
+        predictions, ground_truth = [], []
         for batch in loader:
-            texts = batch["input_ids"].to(device)
-            attention_masks = batch["attention_mask"].to(device)
-            punctuations = batch["labels"].to(device)
+            text_ids, attention_masks, punc_ids = (
+                batch["input_ids"].to(device),
+                batch["attention_mask"].to(device),
+                batch["labels"].to(device),
+            )
 
-            # Forward pass
-            _, logits = model(texts=texts, attention_masks=attention_masks, punctuations=punctuations)
+            # Forward pass (predictions)
+            logits = model(text_ids=text_ids, attention_masks=attention_masks)
 
-            # Flatten punctuations and logits
-            punctuations = punctuations.view(-1)
-            logits = logits.view(-1, num_puncs)
-            predicted_punctuations = torch.argmax(logits, axis=1)
+            # Reshape logits and ground truth
+            logits = logits.view(-1, logits.shape[2]).contiguous()
+            punc_ids = punc_ids.view(-1).contiguous()
 
-            # Ignore predictions made for labels with value -100 (padding tokens)
-            mask = punctuations != 100
-            punctuations = torch.masked_select(punctuations, mask)
-            predicted_punctuations = torch.masked_select(predicted_punctuations, mask)
+            # Convert logits to prediction ids
+            _, pred_punc_ids = torch.max(logits, 1)
 
-            puncs_predictions.extend(list(predicted_punctuations.cpu().numpy()))
-            puncs_correct.extend(list(punctuations.cpu().numpy()))
+            # Remove predictions corresponding to paddings
+            pred_punc_ids = list(pred_punc_ids.cpu().numpy())
+            punc_ids = list(punc_ids.cpu().numpy())
+            pred_punc_ids, punc_ids = _remove_predictions_for_padded_tokens(pred_punc_ids, punc_ids)
+
+            predictions += pred_punc_ids
+            ground_truth += punc_ids
 
     model.train()
 
-    return f1_score(puncs_correct, puncs_predictions, average="micro")
+    return f1_score(ground_truth, predictions, average="micro")
 
 
 def finetune_and_evaluate_model(cfg, dataset_dir, experiment_dir):
@@ -62,76 +80,89 @@ def finetune_and_evaluate_model(cfg, dataset_dir, experiment_dir):
     os.makedirs(experiment_dir, exist_ok=True)
     os.makedirs(checkpoint_dir, exist_ok=True)
 
-    # Instantiate the dataloaders for the train and dev splits
-    train_dataset = BERTPhraseBreakDataset(cfg["bert_model_name"], dataset_dir, split="train")
+    # Instantiate the dataloaders for train/dev splits
+    train_dataset = BERTPhraseBreakDataset(model_name=cfg["bert_model_name"], data_dir=dataset_dir, split="train")
     train_loader = DataLoader(
         train_dataset,
         batch_size=cfg["batch_size"],
         shuffle=True,
+        collate_fn=train_dataset.pad_collate,
         pin_memory=True,
         drop_last=True,
     )
 
-    dev_dataset = BERTPhraseBreakDataset(cfg["bert_model_name"], dataset_dir, split="dev")
+    dev_dataset = BERTPhraseBreakDataset(model_name=cfg["bert_model_name"], data_dir=dataset_dir, split="dev")
     dev_loader = DataLoader(
         dev_dataset,
         batch_size=cfg["batch_size"],
         shuffle=False,
-        pin_memory=False,
-        drop_last=False,
+        collate_fn=dev_dataset.pad_collate,
+        pin_memory=True,
+        drop_last=True,
     )
 
     # Specify the device to be used for training
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # Instantite the model
+    # Instantiate the model
     num_puncs = len(train_dataset.punc_vocab)
-    model = BERTPhraseBreakPredictor(cfg["bert_model_name"], num_puncs=num_puncs)
+    model = BERTPhraseBreakPredictor(model_name=cfg["bert_model_name"], num_puncs=num_puncs)
     model = model.to(device)
 
     # Instantiate the optimizer
-    optimizer = torch.optim.Adam(model.parameters(), lr=cfg["lr"])
-    
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=cfg["lr"])
+
+    # Specify the criterion to fine-tune the model
+    criterion = torch.nn.CrossEntropyLoss()
+
     model.train()
 
     best_dev_F1_score = 0.0
     training_log = []
     for epoch in range(0, cfg["num_epochs"]):
         # Train step
-        puncs_predictions, puncs_correct = [], []
+        predictions, ground_truth = [], []
         for batch in train_loader:
-            texts = batch["input_ids"].to(device)
-            attention_masks = batch["attention_mask"].to(device)
-            punctuations = batch["labels"].to(device)
+            text_ids, attention_masks, punc_ids = (
+                batch["input_ids"].to(device),
+                batch["attention_mask"].to(device),
+                batch["labels"].to(device),
+            )
 
             # Clear all previous gradients
             optimizer.zero_grad()
 
-            # Forward pass
-            loss, logits = model(texts=texts, attention_masks=attention_masks, punctuations=punctuations)
+            # Forward pass (predictions)
+            logits = model(text_ids=text_ids, attention_masks=attention_masks)
 
-            # Backward pass (Gradient computation and weight update)
+            # Reshape logits and ground truth
+            logits = logits.view(-1, logits.shape[2]).contiguous()
+            punc_ids = punc_ids.view(-1).contiguous()
+
+            # Loss computation
+            loss = criterion(logits, punc_ids)
+
+            # Backward pass (Gradient computation, gradient clipping and weight update)
             loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), cfg["max_grad_norm"])
             optimizer.step()
 
-            # Flatten punctuations and logits
-            punctuations = punctuations.view(-1)
-            logits = logits.view(-1, num_puncs)
-            predicted_punctuations = torch.argmax(logits, axis=1)
+            # Convert logits to prediction ids
+            _, pred_punc_ids = torch.max(logits, 1)
 
-            # Ignore predictions made for labels with value -100 (padding tokens)
-            mask = punctuations != 100
-            punctuations = torch.masked_select(punctuations, mask)
-            predicted_punctuations = torch.masked_select(predicted_punctuations, mask)
+            # Remove predictions corresponding to paddings
+            pred_punc_ids = list(pred_punc_ids.cpu().numpy())
+            punc_ids = list(punc_ids.cpu().numpy())
+            pred_punc_ids, punc_ids = _remove_predictions_for_padded_tokens(pred_punc_ids, punc_ids)
 
-            puncs_predictions.extend(list(predicted_punctuations.cpu().numpy()))
-            puncs_correct.extend(list(punctuations.cpu().numpy()))
+            predictions += pred_punc_ids
+            ground_truth += punc_ids
 
-        train_F1_score = f1_score(puncs_correct, puncs_predictions, average="micro")
+        train_F1_score = f1_score(ground_truth, predictions, average="micro")
         train_F1_score = train_F1_score * 100
 
         # Eval step
-        dev_F1_score = _eval(model, device, dev_loader, num_puncs)
+        dev_F1_score = _eval(model, device, dev_loader)
         dev_F1_score = dev_F1_score * 100
 
         # Check if the dev_F1_score is better than the best_dev_F1_score
@@ -145,12 +176,12 @@ def finetune_and_evaluate_model(cfg, dataset_dir, experiment_dir):
 
         # Log training for one epoch
         epoch_log_string = f"Epoch: {epoch + 1}, Train Set F1: {train_F1_score:.2f}, Dev Set F1: {dev_F1_score:.2f}"
-        print(epoch_log_string, flush=True)
+        print(epoch_log_string)
         training_log.append(epoch_log_string)
 
     # Training summary
     summary_log_string = f"Best performing model - Epoch: {best_epoch}, F1 Score on Dev Set: {best_dev_F1_score:.2f}"
-    print(summary_log_string, flush=True)
+    print(summary_log_string)
     training_log.append(summary_log_string)
 
     return training_log
@@ -164,12 +195,10 @@ if __name__ == "__main__":
 
     # Command line arguments
     parser.add_argument(
-        "--config_file", help="Path to file containing the finetuning configuration to be loaded", required=True
+        "--config_file", help="Path to file containing the fine tuning configuration to be loaded", required=True
     )
     parser.add_argument("--dataset_dir", help="Directory containing the processed dataset", required=True)
-    parser.add_argument(
-        "--experiment_dir", help="Directory where the finetuning artifacts will be saved", required=True
-    )
+    parser.add_argument("--experiment_dir", help="Directory where artifacts will be saved", required=True)
 
     # Parse and get the command line arguments
     args = parser.parse_args()
