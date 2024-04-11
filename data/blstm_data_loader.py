70071172454d3eb19e19341db489da4200cafc3b
anandaswarup <anandaswarup.vadapalli@gmail.com>
2024-04-11 12:33:47 +0530

    Data loader for BLSTM token classification model using task specific word embeddings trained from scratch

--- data/blstm_data_loader.py
+++ data/blstm_data_loader.py
@@ -1,9 +1,11 @@
 """Data loader for BLSTM token classification model using task specific word embeddings trained from scratch"""
 
 import os
+
 import torch
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import Dataset
+
 from utils.utils import load_json_to_dict, load_vocab_to_dict, read_dataset_file
 
 
@@ -20,7 +22,9 @@ class PhraseBreakDataset(Dataset):
             split (str): The dataset split to process ("train"/ "dev" / "test")
         """
         vocab_dir = os.path.join(data_dir, "vocab")
-        assert os.path.isdir(vocab_dir), f"Vocab dir does not exist, run build_vocab_blstm.py"
+        assert os.path.isdir(
+            vocab_dir
+        ), "Vocab dir does not exist, run build_vocab_blstm.py"
 
         # Load vocabularies for both words and punctuations
         self.word_vocab = load_vocab_to_dict(os.path.join(data_dir, "vocab/words.txt"))
@@ -29,8 +33,12 @@ class PhraseBreakDataset(Dataset):
         self.params = load_json_to_dict(os.path.join(data_dir, "vocab/params.json"))
 
         # Load sentence / punctuation sequences for the specified split
-        self.sentences = read_dataset_file(os.path.join(data_dir, f"{split}/sentences.txt"))
-        self.punctuations = read_dataset_file(os.path.join(data_dir, f"{split}/punctuations.txt"))
+        self.sentences = read_dataset_file(
+            os.path.join(data_dir, f"{split}/sentences.txt")
+        )
+        self.punctuations = read_dataset_file(
+            os.path.join(data_dir, f"{split}/punctuations.txt")
+        )
         assert len(self.sentences) == len(self.punctuations)
 
         # Setting indices for _UNK_, _PAD_, and _X_ tokens
@@ -43,7 +51,10 @@ class PhraseBreakDataset(Dataset):
     def __getitem__(self, index):
         sentence, punctuations = self.sentences[index], self.punctuations[index]
 
-        text_seq = [self.word_vocab[word] if word in self.word_vocab else self.word_unk_idx for word in sentence]
+        text_seq = [
+            self.word_vocab[word] if word in self.word_vocab else self.word_unk_idx
+            for word in sentence
+        ]
         punc_seq = [self.punc_vocab[punc] for punc in punctuations]
 
         return (torch.LongTensor(text_seq), torch.LongTensor(punc_seq))
@@ -53,7 +64,11 @@ class PhraseBreakDataset(Dataset):
         sentences, punctuations = zip(*batch)
         sentences, punctuations = list(sentences), list(punctuations)
 
-        padded_sentences = pad_sequence(sentences, batch_first=True, padding_value=self.word_pad_idx)
-        padded_punctuations = pad_sequence(punctuations, batch_first=True, padding_value=-1)
+        padded_sentences = pad_sequence(
+            sentences, batch_first=True, padding_value=self.word_pad_idx
+        )
+        padded_punctuations = pad_sequence(
+            punctuations, batch_first=True, padding_value=-1
+        )
 
         return padded_sentences, padded_punctuations
